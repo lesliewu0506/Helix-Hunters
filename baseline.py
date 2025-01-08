a559e20d72f0f958aeadfd24fe5bcb9c2e857d9d
lesliewu0506 <135598464+lesliewu0506@users.noreply.github.com>
2025-01-08 10:20:24 +0100

    Updated Protein and Grid class to match new datastructure, currently editing Rating class.

--- baseline.py
+++ baseline.py
@@ -9,9 +9,8 @@ class Protein():
 
     def _add_folding_structure(self, function):
         self.protein_structure = function(self.protein_sequence)
-
-        self.protein_grid = Grid(self.protein_sequence, self.protein_structure)
-        self.protein_rating = Rating(self.protein_sequence, self.protein_structure, self.protein_grid.grid)
+        self.structure = Grid(self.protein_sequence, self.protein_structure).get_structure()
+        self.protein_rating = Rating(self.protein_sequence, self.protein_structure)
 
     def output_csv(self):
         with open('output.csv', 'w', newline = '') as csvfile:
@@ -23,33 +22,16 @@ class Protein():
 
 class Grid():
     def __init__(self, protein_sequence, protein_structure):
+        self.structure = self._create_structure(protein_sequence, protein_structure)
 
-        self.protein_sequence = protein_sequence
-        self.protein_structure = protein_structure
+    def _create_structure(self, protein_sequence, protein_structure):
+        self.coordinates = {}
+        self.x_coord = 0 
+        self.y_coord = 0
 
-        # Create 2D array with length 2 times sequence length plus 2 buffer
-        self.grid_size = 2 * len(self.protein_sequence) + 2
-
-        self._initialize_grid()
-        self._add_structure_to_grid()
-
-    def _initialize_grid(self):
-        self.grid = []
-        for _ in range(self.grid_size):
-            column = []
-            for _ in range(self.grid_size):
-                column.append(0)
-            self.grid.append(column)
-
-    def _add_structure_to_grid(self):
-        # Define starting point to be in center
-        self.x_coord = self.grid_size // 2
-        self.y_coord = self.grid_size // 2
-
-        # Add points to grid
-        for i in range(len(self.protein_sequence)):
-            self.grid[self.y_coord][self.x_coord] = self.protein_sequence[i]
-            self._update_position(self.protein_structure[i]) 
+        for i in range(len(protein_sequence)):
+            self.coordinates[(self.x_coord, self.y_coord)] = (protein_sequence[i], i)
+            self._update_position(protein_structure[i])
 
     def _update_position(self, direction):
         if direction == 1:
@@ -57,19 +39,18 @@ class Grid():
         elif direction == -1:
             self.x_coord -= 1
         elif direction == 2:
-            self.y_coord -= 1
-        elif direction == -2:
             self.y_coord += 1
-    
-    def get_grid(self):
-        for i in range(self.grid_size):
-            print(self.grid[i])
+        elif direction == -2:
+            self.y_coord -= 1
+
+    def get_structure(self):
+        print(self.structure)
+        return self.structure
 
 class Rating():
-    def __init__(self, protein_sequence, protein_structure, grid):
+    def __init__(self, protein_sequence, protein_structure):
         self.protein_sequence = protein_sequence
         self.protein_structure = protein_structure
-        self.grid = grid
 
         self.score = 0
 
@@ -77,20 +58,28 @@ class Rating():
         self._correction_to_rating()
 
     def _count_adjacent(self):
-        rows = len(self.grid)
-        cols = len(self.grid[0])
+    
+    def _check_neighbour(self, direction):
+        if direction == -1:
+            amino_1 = self.coordinates[self.x_polar, self.y_polar]
+            amino_2 = self.coordinates[self.x_polar - 1, self.y_polar]
+            return self._check_pair(amino_1, amino_2)
+        elif direction == 1:
+            amino_1 = self.coordinates[self.x_polar, self.y_polar]
+            amino_2 = self.coordinates[self.x_polar + 1, self.y_polar]
+            return self._check_pair(amino_1, amino_2)
+        if direction == -2:
+            amino_1 = self.coordinates[self.x_polar, self.y_polar]
+            amino_2 = self.coordinates[self.x_polar, self.y_polar - 1]
+            return self._check_pair(amino_1, amino_2)
+        if direction == 2:
+            amino_1 = self.coordinates[self.x_polar, self.y_polar]
+            amino_2 = self.coordinates[self.x_polar, self.y_polar + 1]
+            return self._check_pair(amino_1, amino_2)
 
-        for row in range(rows):
-            for col in range(cols):
-                if col < cols - 1 and self.grid[row][col] == "C" and self.grid[row][col + 1] == "C":
-                    self.score -= 5
-                elif col < cols - 1 and (self.grid[row][col] in ["H", "C"]) and (self.grid[row][col + 1] in ["H", "C"]):
-                    self.score -= 1
-                if row < rows - 1 and self.grid[row][col] == "C" and self.grid[row + 1][col] == "C":
-                    self.score -= 5
-                elif row < rows - 1 and (self.grid[row][col] in ["H", "C"]) and (self.grid[row + 1][col] in ["H", "C"]):
-                    self.score -= 1
-
+    def _check_pair(self, amino_1, amino_2):
+        if amino_2 in ['H', 'C']:
+            return True
     def _correction_to_rating(self):
         for i in range(1, len(self.protein_sequence)):
             current_amino = self.protein_sequence[i]
@@ -112,7 +101,7 @@ class Rating():
 class Plot():
     def __init__(self, protein: Protein):
         self.protein_sequence = protein.protein_sequence
-        self.protein_structure = protein.protein_structure
+        self.protein_structure = protein.structure
 
         self._assign_coordinates()
 
