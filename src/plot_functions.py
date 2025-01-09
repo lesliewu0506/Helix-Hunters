3598fd97d8fbf79a269397ea5420fc97b395e19c
lesliewu0506 <135598464+lesliewu0506@users.noreply.github.com>
2025-01-09 13:39:07 +0100

    Enhance protein plotting functionality with detailed connection visualizations and improved legend representation.

--- src/plot_functions.py
+++ src/plot_functions.py
@@ -12,6 +12,13 @@ class Plot():
         self.plot_structure()
         
     def plot_structure(self) -> None:
+        """
+        Plots the entire protein structure with the amino acids,
+        sequential connections and polar connections.
+        Amino acids are colored dots; H: red; P: blue; C: green.
+        Sequential connections are black lines.
+        Polar connections are highlighted by colored dashed lines.
+        """
         plt.figure(figsize = (10, 10))
 
         color_map = {'H' : 'red', 'P' : 'blue', 'C' : 'green'}
@@ -40,8 +47,7 @@ class Plot():
         self._plot_polar_connections()
             
         # Dummy plot for legend
-        for amino_type, colour in color_map.items():
-           plt.scatter([], [], color=colour, label = amino_type)
+        self._plot_legend(color_map)
 
         plt.title('2D Protein Plot')
         plt.legend(loc = 'best')
@@ -49,6 +55,7 @@ class Plot():
         plt.show()
 
     def _plot_sequential_connections(self) -> None:
+        """Plots the sequential connections of the protein with a black line."""
         x_prev, y_prev = 0, 0 
         for i, (x, y) in enumerate(self.structure):
             if i == 0:
@@ -58,6 +65,13 @@ class Plot():
                 x_prev, y_prev = x, y
     
     def _plot_polar_connections(self) -> None:
+        """
+        Plots the polar connections of the protein.
+        Type of connections is highlighted by the color of the dashed line.
+        grey: H-H connection
+        lightgreen: C-H connection
+        darkorange: C-C connection
+        """
         # Mapping for all neighbouring points
         direction_map = {(1, 0) , (-1, 0), (0, 1), (0, -1)}
 
@@ -74,7 +88,7 @@ class Plot():
                     # Check for valid and non sequential points
                     if (x_next, y_next) in self.structure and not self._check_sequential(x_current, y_current, x_next, y_next):
                         amino_2 = self.structure[(x_next, y_next)][0]
-                        
+
                         color = self._check_connection_type(amino_1, amino_2)
                         self._helper_plot_polar_(x_current, y_current, x_next, y_next, color)
 
@@ -86,15 +100,31 @@ class Plot():
         i = self.structure[(x_old, y_old)][1]
         return self.structure[(x_new, y_new)][1] in [(i - 1), (i + 1)]
 
-    def _check_connection_type(self, amino_1: str, amino_2: str) -> str:
+    def _check_connection_type(self, amino_1: str, amino_2: str) -> str | None:
         """Checks the pair for possible connections and returns the type of connection with color."""
         if (amino_1 == 'H' and amino_2 == 'H'):
             return "grey"
         elif (amino_1 == 'H' and amino_2 == 'C') or (amino_1 == 'C' and amino_2 == 'H'):
             return "lightgreen"
         elif amino_1 == 'C' and amino_2 == 'C':
-            return "green"
-        return "white"
+            return "darkorange"
+        return None
+
+    def _helper_plot_polar_(self, x_old: int, y_old: int, x_new: int, y_new: int, colour: str | None) -> None:
+        """Helper functions that plots the polar connections."""
+        if colour is not None:
+            plt.plot([x_old, x_new], [y_old, y_new], color = colour, linestyle = '--', linewidth = 2, zorder = 1)
     
-    def _helper_plot_polar_(self, x_old: int, y_old: int, x_new: int, y_new: int, colour: str) -> None:
-        plt.plot([x_old, x_new], [y_old, y_new], color = colour, linestyle = '--', linewidth = 2, zorder = 1)
+    def _plot_legend(self, color_map: dict[str, str]) -> None:
+        """
+        Plots the legend manually with dummy plots.
+        It adds the labels for the colored amino acids.
+        It also adds the type of connections between polar amino acids.
+        """
+        # Label polar connections with dashed colored lines
+        plt.plot([], [], color = "grey", linestyle = '--', label = "H-H Connection")
+        plt.plot([], [], color = "lightgreen", linestyle = '--', label = "H-C Connection")
+        plt.plot([], [], color = "darkorange", linestyle = '--', label = "C-C Connection")
+
+        for amino_type, colour in color_map.items():
+           plt.scatter([], [], color=colour, label = amino_type)
