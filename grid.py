0cb4f485bc95a432b7ae7df50d3913631d5a9f49
lesliewu0506 <135598464+lesliewu0506@users.noreply.github.com>
2025-01-09 11:35:38 +0100

    Split the classes into multiple files.

---
+++ grid.py
@@ -0,0 +1,35 @@
+class Grid():
+    """
+    A class to represent a structure of a protein sequence.
+    It contains self.structure, a dict with:
+    - keys (x, y): coordinates of an amino acid.
+    - values (type, order): type of amino acid and the order in the chain.
+    """
+
+    def __init__(self, protein_sequence: str, amino_directions: list[int]) -> None:
+        self.structure: dict[tuple[int, int], tuple[str, int]] = self._create_structure(protein_sequence, amino_directions)
+
+    def _create_structure(self, protein_sequence: str, amino_directions: list[int]) -> dict[tuple[int, int], tuple[str, int]]:
+        """Builds the dictionary structure and returns the structure."""
+        structure: dict[tuple[int, int], tuple[str, int]] = {}    
+        x_current: int = 0 
+        y_current: int = 0
+
+        for i in range(len(protein_sequence)):
+            # Adds coordinates as keys with values [type of amino, order in chain]
+            structure[(x_current, y_current)] = (protein_sequence[i], i)
+            x_current, y_current = self._update_position(x_current, y_current, amino_directions[i])
+
+        return structure
+    
+    def _update_position(self, x_old: int, y_old: int, direction: int) -> tuple[int, int]:
+        """Updates x and y based on direction."""
+        # Maps direction to a change in x and y
+        direction_map = {0 : (0, 0), 1 : (1, 0), -1 : (-1, 0), 2: (0, 1), -2: (0, -1)}
+
+        dx, dy = direction_map[direction]
+        return x_old + dx, y_old + dy
+
+    def get_structure(self) -> dict[tuple[int, int], tuple[str, int]]:
+        """Returns the structure of the protein."""
+        return self.structure
---
+++ protein.py
@@ -0,0 +1,41 @@
+import csv
+from grid import Grid
+from rating import Rating
+from typing import Callable
+
+class Protein():
+    """
+    A class to represent a protein and its attributes.
+    It stores a protein sequence and uses a provided folding function
+    to generate a folding pattern (amino_directions). Then it creates
+    a structure (mapping of positions to amino acids) and computes
+    the total rating of the protein.
+    """
+
+    def __init__(self, protein_sequence: str, function: Callable[[str], list[int]]) -> None:
+        self.protein_sequence: str = protein_sequence
+        self.amino_directions: list[int] = []
+        self.structure: dict[tuple[int, int], tuple[str, int]] = {}
+        self.protein_rating: int = 0
+        self._build_structure(function)
+
+    def _build_structure(self, function: Callable[[str], list[int]]) -> None:
+        """Creates the attributes for the protein with specific fold."""
+        self.amino_directions = function(self.protein_sequence)
+
+        self.structure = Grid(self.protein_sequence, self.amino_directions).get_structure()
+        self.protein_rating = Rating(self.protein_sequence, self.structure).get_rating()
+
+    def output_csv(self) -> None:
+        """Creates a csv file containing the amino acids and their fold."""
+        with open('output.csv', 'w', newline = '') as csvfile:
+            writer = csv.writer(csvfile)
+
+            # Mandatory header line
+            writer.writerow(['amino', 'fold'])
+
+            for amino, direction in zip(self.protein_sequence, self.amino_directions):
+                writer.writerow([amino, direction])
+
+            # Mandatory footer line
+            writer.writerow(['score', self.protein_rating])
