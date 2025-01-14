357c980e40cc1e26343984fd272fbe3c1fc55062
lesliewu0506 <135598464+lesliewu0506@users.noreply.github.com>
2025-01-14 16:56:59 +0100

    Cleaned up main function for baseline purposes.

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-from src.brute_force.Brute_Force import generate_all_foldings, brute_force
+from src.brute_force.brute_force import generate_all_foldings, brute_force
+
+protein_sequences = ["HHPHHHPHPHHHPH",
+                     "HPHPPHHPHPPHPHHPPHPH",
+                     "PPPHHPPHHPPPPPHHHHHHHPPHHPPPPHHPPHPP",
+                     "HHPHPHPHPHHHHPHPPPHPPPHPPPPHPPPHPPPHPHHHHPHPHPHPHH",
+                     "PPCHHPPCHPPPPCHHHHCHHPPHHPPPPHHPPHPP",
+                     "CPPCHPPCHPPCPPHHHHHHCCPCHPPCPCHPPHPC",
+                     "HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH",
+                     "HCPHPHPHCHHHHPCCPPHPPPHPPPPCPPPHPPPHPHHHHCHPHPHPHH"]
 
 if __name__ == "__main__":
-    brute_force("HHPHHHPHPHHHPH", save = True)
+    # =========================== Brute Force ==========================
+    # NOTE: This function is good only for the first protein sequence.
+    # Running it for the others results in huge data files (>5GB). 
+    # This is only for demonstration purposes.
+
+    # # Generate all possible folding structures
+    # generate_all_foldings(protein_sequences[0])
+    # # Try all possible combinations
+    # brute_force(protein_sequences[0], save = True)
+    pass
