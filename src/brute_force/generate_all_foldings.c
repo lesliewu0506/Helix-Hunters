623ec0496778ba9dc904e0a5d887aa70e6915ab4
lesliewu0506 <135598464+lesliewu0506@users.noreply.github.com>
2025-01-18 14:54:27 +0100

    Added prototype, gotten length of proteins sequence and created csv file for specific folding.

--- src/brute_force/generate_all_foldings.c
+++ src/brute_force/generate_all_foldings.c
@@ -3,11 +3,14 @@
 #include <stdbool.h>
 #include <string.h>
 
+// Function prototypes
+void generate_all_foldings(const char* protein_sequence);
+
 // Main
 int main(int argc, char** argv)
 {
     // For invalid commands
-    if(argc < 2)
+    if(argc != 2)
     {
         printf("Usage: %s <protein_sequence>\n", argv[0]);
         return 1;
@@ -19,5 +22,20 @@ int main(int argc, char** argv)
 
 void generate_all_foldings(const char* protein_sequence)
 {
+    // Get length of sequence
+    int length = strlen(protein_sequence);
     
+    // Create CSV file
+    char filename[256];
+    sprintf(filename, "%s.csv", protein_sequence);
+    FILE * file = fopen(filename, "w");
+
+    // Check for error in opening file
+    if (!file)
+    {
+        printf("Error while opening file.\n");
+        return;
+    }
+
+
 }
