158129a82c42bb6aa16c70cb255be2274eeb487f
lesliewu0506 <135598464+lesliewu0506@users.noreply.github.com>
2025-01-18 16:04:07 +0100

    Implemented direction translator for translating relative directions into absolute directions.

--- src/brute_force/generate_all_foldings.c
+++ src/brute_force/generate_all_foldings.c
@@ -3,8 +3,20 @@
 #include <stdbool.h>
 #include <string.h>
 
+// Direction Mapping
+static const int direction_map[4][3] = 
+{
+    { 2,  1, -2},
+    {-2, -1,  2},
+    {-1,  2,  1},
+    { 1, -2, -1}
+};
+
 // Function prototypes
 void generate_all_foldings(const char* protein_sequence);
+int* check_folding(int* rel_dir, int protein_length);
+void direction_translator(int* rel_dir, int* absolute, int relative_length);
+int direction_to_row(int direction);
 
 // Main
 int main(int argc, char** argv)
@@ -23,10 +35,10 @@ int main(int argc, char** argv)
 void generate_all_foldings(const char* protein_sequence)
 {
     // Get length of sequence
-    int length = strlen(protein_sequence);
+    int protein_length = strlen(protein_sequence);
     
     // Create CSV file
-    char filename[256];
+    char filename[60];
     sprintf(filename, "%s.csv", protein_sequence);
     FILE* file = fopen(filename, "w");
 
@@ -39,35 +51,34 @@ void generate_all_foldings(const char* protein_sequence)
 
     // Calculate total amount of combinations
     unsigned long long int total = 1;
-    for (int i = 0; i < length - 2; i++)
+    for (int i = 0; i < protein_length - 2; i++)
     {
         total *= 3;
     }
 
     // Create buffer to store relative directions
-    int* relative_directions = (int*)malloc(sizeof(int) * (length - 2));
+    int* relative_directions = (int*)malloc(sizeof(int) * (protein_length - 2));
 
     // Turn every value into base 3 to get all relative direction combinations
     for (unsigned long long int value = 0; value < total; value++)
     {
         unsigned long long int temp = value;
-        for (int i = length - 3; i >= 0; i--)
+        for (int i = protein_length - 3; i >= 0; i--)
         {
             relative_directions[i] = temp % 3;
             temp /= 3;
         }
-        // Add first and last amino acid directions
-        relative_directions[0] = 1;
-        relative_directions[-1] = 0;
-        int* result = relative_directions;
+
+        // Get absolute directions
+        int* result = check_folding(relative_directions, protein_length);
 
         // Write to CSV
         if (result != NULL)
         {
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < protein_length; i++)
             {
                 fprintf(file, "%i", result[i]);
-                if (i < length - 1)
+                if (i < protein_length - 1)
                 {
                     fprintf(file, ",");
                 }
@@ -79,4 +90,50 @@ void generate_all_foldings(const char* protein_sequence)
     // Close file and free memory
     fclose(file);
     free(relative_directions);
+}
+
+// Translates relative directions into absolute directions and then checks if it is valid
+int* check_folding(int* relative_directions, int protein_length)
+{
+    // Create array for only in this function
+    static int absolute_directions[60];
+
+    int relative_length = protein_length - 2;
+    direction_translator(relative_directions, absolute_directions, relative_length);
+
+    return absolute_directions;
+}
+
+// Translates array from relative directions to absolute directions
+void direction_translator(int* relative_directions, int* absolute_directions, int relative_length)
+{
+    // Start with first direction
+    int current_direction = 1;
+    absolute_directions[0] = current_direction;
+
+    for (int i = 0; i < relative_length; i++)
+    {
+        int row = direction_to_row(current_direction);
+        int relative_direction = relative_directions[i];
+        int absolute_direction = direction_map[row][relative_direction];
+
+        absolute_directions[i + 1] = absolute_direction;
+        current_direction = absolute_direction;
+    }
+
+    // Add final direction
+    absolute_directions[relative_length + 1] = 0;
+}
+
+// Helper function for translating absolute direction to index row for mapping
+int direction_to_row(int direction) 
+{
+    // Use switch instead of if else statements
+    switch(direction) 
+    {
+        case 1:  return 0;
+        case -1: return 1;
+        case 2:  return 2;
+        case -2: return 3;
+    }
 }
