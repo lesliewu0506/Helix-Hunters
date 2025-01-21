733887ea940e46baac18d86d3c30e157931c9de6
Lara Ersoy <laraersoy@gmail.com>
2025-01-21 14:05:22 +0100

    aanpassing genetic algoritme code

--- src/algorithms/genetic_2.py
+++ src/algorithms/genetic_2.py
@@ -1,4 +1,7 @@
 import random as rd
+import matplotlib as plt
+import numpy as np
+from prettytable import PrettyTable
 
 from src.utils.helpers import random_fold
 from src.classes.protein import Protein
@@ -12,7 +15,7 @@ class Genetic_Algorithm():
         super().__init__(protein_sequence)
 
     def run(self, show_plot: bool = False, save_plot: bool = False, save_data: bool = False, repeats: int = 1, iterations: int = 10000) -> None:
-        """The Genetic algorithm generates sequences for a protein and calculates the scores"""
+        """The Genetic algorithm generates sequences for a protein and calculates the scores
         self.run_algorithm(
             algorithm = "Genetic",
             show_plot = show_plot,
@@ -22,9 +25,51 @@ class Genetic_Algorithm():
             iterations = iterations,
             algorithm_function = self._greedy_iterated)
         
-    def ted(self, n: int) -> None:
-s
+    def initialize_population(target):
+        population = list()
+        target_length = len(target)
 
+        for i in range(population_size):
+            temperature = list()
+            for j in range(target_length):
+                temperature.append(random.choice(Genes))
+            population.append(temperature)
+
+        return population
+    
+    def generate_crossover(selected_chromo, chromo_length, population):
+        offspring_cross = []
+        for i in range(int(population_size)):
+            parent_1= random.choice(selected_chromo)
+            parent_2 = random.choice(population[:int(population_size*50)])
+
+            p_1 = parent_1[0]
+            p_2 = parent_2[0]
+
+            cross_point = random.randit(1, chromo_length-1)
+            child = p_1[:crossover_point] + p_2[crossover_point:]
+            offspring_cross.extend([child])
+        return offspring_cross
+    
+    def apply_mutate(offspring, mutation_rate):
+        mutated_offspring = []
+
+
+
+"""
+
+def evaluated_fit(parameters):
+    a, b, c = parameters
+    if a <= 0:
+        return = -float('inf')
+    vertex_x = -b / (2*a) 
+    vertex_y = a * (vertex_x ** 2) + b * vertex_x + c
+    y_left = a * (-1) ** 2 + b * (-1) + c
+    y_right = a * (-1) ** 2 + b * (1) + c
+    curviness = abs(y_left - vertex_y) + abs(y_right - vertex_y)
+    return -curviness
+
+def create_initial_population(size, lower_bound, upper_bound):
 
 
 initialized_population()
